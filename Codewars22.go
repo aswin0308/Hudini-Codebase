31da03f6015d85fd3f1316a8ef36de8925ce9363
aswin0308 <aswinbaiju77@gmail.com>
2024-05-23 23:10:26 +0530

    array index

--- Codewars22.go
+++ Codewars22.go
@@ -58,12 +58,12 @@
 //   for len(numstr)>len(message){
 //     message = "#"+message
 //   }
-  
+
 //   for i:= range numstr{
 //     if(numstr[i]=='1' && message[i]!= '#'){
 //       newmsg += string(message[i])
 //     }
-    
+
 //   }
 // 	return newmsg
 // }
@@ -73,7 +73,7 @@
 // 	fmt.Println(result)
 // }
 
-// package main 
+// package main
 
 // import "fmt"
 // func SequenceSum(start, end, step int) int {
@@ -93,8 +93,6 @@
 // 	fmt.Println(result)
 //   }
 
-
-
 // Multiples of 3 or 5
 
 // func Multiple3And5(number int) int {
@@ -111,34 +109,85 @@
 // 	return sum
 //   }
 
+// package main
+
+// import "math"
+
+// import ("fmt"
+//         "math")
+
+// func FindNextPower(val, pow int) int {
+// 	num:= 0
+
+// iterate from 1 to val
+// calculate cube of i every turn
+// compare if cube of i is greater than value
+// if yes,
+// return cube of i
+
+// 	for i:=1;i<val;i++ {
+// 		j :=1
+// 		num:= 1
+// 		for j<=pow{
+// 				num *= i
+// 				j++
+// 					}
+// 	  if(num > val){
+// 		return num
+// 	  }
+// 	}
+// 	return num
+//   }
+
+//	  func main(){
+//		result := FindNextPower(12385, 3)
+//		fmt.Println(result)
+//		fmt.Println("Cube root of is", math.Pow(25, 1.0/3.0))
+//	  }
 package main
+
 import "fmt"
 
-func FindNextPower(val, pow int) int {
-	num:= 0
+// import("fmt"
+//          "math")
 
-	// iterate from 1 to val
-	// calculate cube of i every turn
-	// compare if cube of i is greater than value
-	// if yes,
-		// return cube of i
+//   func FindNextPower(val, pow int) int {
+// 	num := 1.0
+// 	root := math.Pow(float64(val), 1.0/float64(pow))
+// 	upper := math.Ceil(root)
+// 	j:=1
+// 	for j<=pow{
+// 				num *= upper
+// 				j++
+// 			  }
+// 	return int(num)
+//     }
+//   func main(){
+// 		result := FindNextPower(1185921, 4)
+// 		fmt.Println(result)
+// 	  }
 
 
-	for i:=1;i<val;i++ {
-		j :=1
-		num:= 1
-		for j<=pow{
-				num *= i
-				j++
-					}
-	  if(num > val){
-		return num
-	  }
-	}
-	return num
+
+func FindEvenIndex(arr []int) int {
+  for i:=0; i<len(arr); i++{
+    sum:=0
+    for j := 0; j < i; j++ {
+            sum += arr[j]
+        }
+  sum2:=0
+  for j:=i+1; j<len(arr); j++{
+    sum2+= arr[j]
   }
+    if(sum == sum2){
+      return i
+    }
+    }
+  return -1
+}
 
-  func main(){
-	result := FindNextPower(12385, 3)
+func main(){
+	result:= FindEvenIndex([]int{8, 0})
 	fmt.Println(result)
-  }
+}
+	  
